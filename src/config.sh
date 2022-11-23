1fca2687f4d59d25e973a1101095092cb06f7f82
Halit Ogunc <hltognc@gmail.com>
2022-11-23 11:53:45 +0100

    chore: remove franciscolourenco/done configs

--- src/config.sh
+++ src/config.sh
@@ -14,8 +14,4 @@ df_config() {
   if command -v dconf > /dev/null 2>&1; then
     dconf write /apps/guake/style/font/palette-name "'Argonaut'" 2> /dev/null
   fi
-
-  # franciscolourenco/done
-  fish -c "set -U __done_min_cmd_duration 10000" # default: 5000 ms
-  fish -c "set -U __done_exclude 'git (?!push|pull)'" # default: all git commands, except push and pull. accepts a regex.
 }
